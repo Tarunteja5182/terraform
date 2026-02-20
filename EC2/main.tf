bcadfa0ef3a6240e362d43e9a9cec60794fedfa8
Tarunteja <taruntejamahanthi@gmail.com>
2026-02-20 21:38:12 +0530

    ec2-changes

--- EC2/main.tf
+++ EC2/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "test_terraform"{
     ami = "ami-0220d79f3f480ecf5"
     instance_type= "t3.micro"
     vpc_security_group_ids = [aws_security_group.test_sg.id]
+    
     tags = {
     Name = "terraform-ec2"
     Env = "sample"
