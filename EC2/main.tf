28b0430ba9dbad98c7226fc51a1fea9294f8838f
Tarunteja <taruntejamahanthi@gmail.com>
2026-02-20 16:03:36 +0530

    adding SG

--- EC2/main.tf
+++ EC2/main.tf
@@ -7,3 +7,22 @@ resource "aws_instance" "test_terraform"{
 
   }
 }
+
+resource "aws_security_group" "test_sg" {
+  name        = "allow-all-terraform" # this is for AWS account
+  description = "Allow TLS inbound traffic and all outbound traffic"
+ egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
